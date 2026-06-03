15ce767248c712aa2a4df09a119b8ae2a0989602
Utkarsh Kankal <utkarsh63.kankal@gmail.com>
2026-06-03 20:14:08 +0530

    feat: implemented conifg and logger using winston

---
+++ lib/config.ts
@@ -0,0 +1,30 @@
+export const config = {
+    node_env: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || '5000', 10),
+
+    postgres: {
+        host: process.env.PG_HOST || 'localhost',
+        port: parseInt(process.env.PG_PORT || '5432', 10),
+        database: process.env.PG_DATABASE || 'api_monitoring',
+        user: process.env.PG_USER || 'postgres',
+        password: process.env.PG_PASSWORD || 'postgres',
+    },
+
+    rabbitmq: {
+        url: process.env.RABBITMQ_URL || 'amqp://localhost:5672',
+        queue: process.env.RABBITMQ_QUEUE || 'api_hits',
+        publisherConfirms: process.env.RABBITMQ_PUBLISHER_CONFIRMS === 'true' || false,
+        retryAttempts: parseInt(process.env.RABBITMQ_RETRY_ATTEMPTS || '3', 10),
+        retryDelay: parseInt(process.env.RABBITMQ_RETRY_DELAY || '1000', 10),
+    },
+
+    jwt: {
+        secret: process.env.JWT_SECRET || "how's_the_monsoon",
+        expiresIn: process.env.JWT_EXPIRES_IN || '24h',
+    },
+
+    rateLimit: {
+        windowsMs: parseInt(process.env.RATE_LIMIT_WINDOWS_MS || '900000', 10),
+        maxRequests: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS || '1000', 10),
+    }
+}
---
+++ lib/logger.ts
@@ -0,0 +1,28 @@
+import winston from "winston";
+import { config } from "./config"
+
+export const logger = winston.createLogger({
+    level: config.node_env === "production" ? "info" : "debug",
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json(),
+    ),
+
+    defaultMeta: { service: "api-monitoring" },
+
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+    ]
+})
+
+if (config.node_env != "production") {
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+        )
+    }))
+}
