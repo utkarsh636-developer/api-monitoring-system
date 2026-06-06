5b008a8ceac313dced202dcf2619ba2ef448e0d3
Utkarsh Kankal <utkarsh63.kankal@gmail.com>
2026-06-06 16:12:53 +0530

    fix: added semicolons instead of commas in constructor(cleaner style)

--- utils/AppError.ts
+++ utils/AppError.ts
@@ -4,10 +4,10 @@ export class AppError extends Error{
     public readonly isOperational: boolean;
     
     constructor(message: string, statusCode: number = 500, errors: any = null){
-        super(message),
-        this.statusCode = statusCode,
-        this.errors = errors,
-        this.isOperational = true,
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.isOperational = true;
         Error.captureStackTrace(this, this.constructor)
     }
 }
