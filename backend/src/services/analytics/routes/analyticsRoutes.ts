7a234829a324bfd752f570e9f1ebbb12552449e5
Utkarsh Kankal <utkarsh63.kankal@gmail.com>
2026-06-20 22:27:41 +0530

    feat: add analytics route for fetching API statistics with authentication middleware

---
+++ backend/src/services/analytics/routes/analyticsRoutes.ts
@@ -0,0 +1,10 @@
+import express, { Router } from 'express';
+import analyticsContainer from '../Dependencies/dependencies';
+import authenticate from '../../../shared/middlewares/authenticate';
+
+const router: Router = express.Router();
+const { analyticsController } = analyticsContainer.controllers;
+
+router.get("/stats", authenticate, (req, res, next) => analyticsController.getStats(req, res, next));
+
+export default router;
